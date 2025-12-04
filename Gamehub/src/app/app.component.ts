b89f11380e20d2d482b518e4133ecfec1b4a91e6
Aaron <hellmulleraaron@gmail.com>
2025-12-04 19:14:02 +0100

    header

--- Gamehub/src/app/app.component.ts
+++ Gamehub/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class App {
   protected readonly title = signal('Gamehub');
